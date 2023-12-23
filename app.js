2212e3a818aa18a31b70e5fa5ea3c0e9724acbf3
killerkc12 <kiranchavankc12@gmail.com>
2023-12-24 00:55:36 +0530

    Add cors on server

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require("cors");
 const app = express()
 const mongoose = require('mongoose')
 const {MONGOURI} = require('./config/keys')
@@ -17,7 +18,21 @@ mongoose.connection.on("connected",()=>{
 require('./models/user')
 require('./models/post')
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({ origin: `${process.env.CLIENT_URL}`, credentials: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
