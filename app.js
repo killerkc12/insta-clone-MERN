2cb9e2246e14ebd4f637e4cb9d6f5f46132a957f
killerkc12 <kiranchavankc12@gmail.com>
2023-12-24 14:35:29 +0530

    Cors changes

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const mongoose = require('mongoose')
 const {MONGOURI} = require('./config/keys')
 const PORT = process.env.PORT || 5000
 
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// app.use(cors({ origin: `${process.env.CLIENT_URL}`, credentials: true }));
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
@@ -17,22 +33,6 @@ mongoose.connection.on("connected",()=>{
 
 require('./models/user')
 require('./models/post')
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(cors({ origin: `${process.env.CLIENT_URL}`, credentials: true }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
